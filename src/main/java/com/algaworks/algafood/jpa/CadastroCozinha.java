45c11cb51e1887bcb0f33beb59ad0b6c74e467ca
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-04-22 15:54:33 -0300

    3.10. Buscando um objeto pelo id no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -15,12 +15,15 @@ public class CadastroCozinha {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	public List<Cozinha> listar() {
-		return manager.createQuery("from Cozinha", Cozinha.class)
-				.getResultList();
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
-	
+
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+
 	@Transactional
 	public Cozinha adicionar(Cozinha cozinha) {
 		return manager.merge(cozinha);
