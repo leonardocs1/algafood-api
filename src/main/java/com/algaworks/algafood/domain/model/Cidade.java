3aabc4b6c1eb06db40bf39fb3c50bd2bed0d628e
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-05-16 09:18:49 -0300

    6.14. Resolvendo o Problema do N+1 com fetch join na JPQL

--- src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +25,7 @@ public class Cidade {
 	@Column(nullable = false)
 	private String nome;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "estado_id", nullable = false)
 	private Estado estado;
 }
--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -41,6 +41,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
@@ -59,7 +60,7 @@ public class Restaurante {
 	@Embedded
 	private Endereco endereco;
 
-	@JsonIgnore
+//	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
