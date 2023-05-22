ce53031abc80971e6edc1a99ce2aab25a6990dcf
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-05-22 19:34:41 -0300

    8.3. Lançando exceções do tipo ResponseStatusException

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
