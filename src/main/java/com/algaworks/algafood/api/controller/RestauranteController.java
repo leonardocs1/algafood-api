c142250c880c86e54d094aa03e42d79293986c3a
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-06-23 18:05:28 -0300

    9.9. Desafio: adicionando constraints de validação no modelo (Resolvido)

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -70,7 +70,7 @@ public class RestauranteController {
 	}
 
 	@PutMapping("{restauranteId}")
-	public Restaurante atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+	public Restaurante atualizar(@PathVariable Long restauranteId, @Valid @RequestBody Restaurante restaurante) {
 
 		try {
 			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
