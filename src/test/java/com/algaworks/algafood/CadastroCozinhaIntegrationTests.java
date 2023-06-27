ec290a15fee3de99ca2665f8f983828c8ff02b98
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-06-26 23:48:41 -0300

    10.5. Desafio: escrevendo testes de integração (Resolvido)

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
@@ -42,6 +44,29 @@ class CadastroCozinhaIntegrationTests {
 		});
 
 		assertThat(erroEsperado).isNotNull();
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+
+		EntidadeEmUsoException erroEsperado = Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
+			cadastroCozinha.excluir(1L);
+		});
+
+		assertThat(erroEsperado).isNotNull();
+
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaExistente() {
+
+		CozinhaNaoEncontradaException erroEsperado = Assertions.assertThrows(CozinhaNaoEncontradaException.class,
+				() -> {
+					cadastroCozinha.excluir(80L);
+				});
+
+		assertThat(erroEsperado).isNotNull();
+
 	}
 
 }
