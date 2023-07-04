c5bae41c75a4f9a104646123847d6b4e047fb4ce
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-07-03 22:18:58 -0300

    11.11. Criando DTOs para entrada de dados na API

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -14,7 +14,7 @@ import com.algaworks.algafood.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
-	@Query("from Restaurante r join r.cozinha left join fetch r.formasPagamento")
+	@Query("from Restaurante r join r.cozinha left join fetch r.cozinha")
 	List<Restaurante> findAll();
 	
 	List<Restaurante> findBytaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
