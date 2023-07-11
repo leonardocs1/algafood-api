e0acc6e9426cfcff2a7e8fb9db113fec2294ffb8
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-07-11 19:30:17 -0300

    12.20. Otimizando a query de pedidos e retornando model resumido na listagem

--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -1,9 +1,13 @@
 package com.algaworks.algafood.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 
 import com.algaworks.algafood.domain.model.Pedido;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+public interface PedidoRepository extends CustomJpaRepository<Pedido, Long> {
 
+	@Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
+	List<Pedido> findAll();
 }
