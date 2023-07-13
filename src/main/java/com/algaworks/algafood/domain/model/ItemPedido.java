18af92b20b8c32623d4386c077d8255a00467179
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-07-13 12:01:45 -0300

    12.21. Desafio: Implementando o endpoint de emissão de pedidos (Parcial)

--- src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -21,17 +21,32 @@ public class ItemPedido {
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private Integer quantidade;
 	private BigDecimal precoUnitario;
 	private BigDecimal precoTotal;
 	private String observacao;
-	
+
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	Pedido pedido;
-	
+
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private Produto produto;
+
+	public void calcularPrecoTotal() {
+		BigDecimal precoUnitario = this.getPrecoUnitario();
+		Integer quantidade = this.getQuantidade();
+
+		if (precoUnitario == null) {
+			precoUnitario = BigDecimal.ZERO;
+		}
+
+		if (quantidade == null) {
+			quantidade = 0;
+		}
+
+		this.setPrecoTotal(precoUnitario.multiply(new BigDecimal(quantidade)));
+	}
 }
