a2e44a7342025408cc532e46559fc704d2a1c404
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-07-14 22:51:39 -0300

    12.23. Desafio: implementando endpoints de transição de status de pedidos (Resolvido)

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -22,11 +22,39 @@ public class FluxoPedidoService {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
 
 		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-			throw new NegocioException(String.format("Status do pedido %d não pode ser alterado de %s para %s",
+			throw new NegocioException(String.format("Status do pedido '%d' não pode ser alterado de '%s' para '%s'",
 					pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CRIADO.getDescricao()));
 		}
 
 		pedido.setStatus(StatusPedido.CONFIRMADO);
 		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
+
+	@Transactional
+	public void cancelar(Long pedidoId) {
+
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+
+		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(String.format("Status do pedido '%d' não pode ser alterado de '%s' para '%s'",
+					pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CANCELADO.getDescricao()));
+		}
+
+		pedido.setStatus(StatusPedido.CANCELADO);
+		pedido.setDataCancelamento(OffsetDateTime.now());
+	}
+
+	@Transactional
+	public void entregar(Long pedidoId) {
+
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+
+		if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
+			throw new NegocioException(String.format("Status do pedido '%d' não pode ser alterado de '%s' para '%s'",
+					pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.ENTREGUE.getDescricao()));
+		}
+
+		pedido.setStatus(StatusPedido.ENTREGUE);
+		pedido.setDataEntrega(OffsetDateTime.now());
 	}
 }
