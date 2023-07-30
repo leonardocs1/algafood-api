a1fad134ab3c35b730c10bebfd3e2a0d6e0b22eb
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-07-30 19:45:05 -0300

    15.5. Processando template do corpo de e-mails com Apache FreeMarker

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -23,7 +23,8 @@ public class FluxoPedidoService {
 		pedido.confirmar();
 
 		var mensagem = Mensagem.builder().assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
-				.corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+				.corpo("pedido-confirmado.html")
+				.variavel("pedido", pedido)
 				.destinatario(pedido.getCliente().getEmail()).build();
 
 		envioEmail.enviar(mensagem);
