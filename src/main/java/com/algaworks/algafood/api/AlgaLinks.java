29b77c5ea92ce6b257b1aa40e5a9870cf7eac9c2
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-08-23 15:08:06 -0300

    19.37. Desafio: implementando endpoint com links de recursos de estatisticas (Resolvido)

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.CozinhaController;
 import com.algaworks.algafood.api.controller.EstadoController;
+import com.algaworks.algafood.api.controller.EstatisticasController;
 import com.algaworks.algafood.api.controller.FluxoPedidoController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.controller.GrupoController;
@@ -230,11 +231,11 @@ public class AlgaLinks {
 	public Link linkToGrupos() {
 		return linkToGrupos(IanaLinkRelations.SELF_VALUE);
 	}
-	
+
 	public Link linkToUsuarioGrupoAssociacao(Long usuarioId, String rel) {
 		return linkTo(methodOn(UsuarioGrupoController.class).associar(usuarioId, null)).withRel(rel);
 	}
-	
+
 	public Link linkToUsuarioGrupoDesassociacao(Long usuarioId, Long grupoId, String rel) {
 		return linkTo(methodOn(UsuarioGrupoController.class).desassociar(usuarioId, grupoId)).withRel(rel);
 	}
@@ -269,6 +270,24 @@ public class AlgaLinks {
 
 	public Link linkToResponsaveisRestaurante(Long restauranteId) {
 		return linkToResponsaveisRestaurante(restauranteId, IanaLinkRelations.SELF_VALUE);
+	}
+
+	public Link linkToEstaristicas(String rel) {
+		return linkTo(EstatisticasController.class).withRel(rel);
+	}
+
+	public Link linkToEstatisticasVendasDiarias(String rel) {
+		TemplateVariables filtroVariables = new TemplateVariables(
+				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM),
+				new TemplateVariable("timeOffset", VariableType.REQUEST_PARAM));
+
+		String pedidoUrl = linkTo(methodOn(EstatisticasController.class).consultarVendasDiarias(null, null)).toUri()
+				.toString();
+
+		return Link.of(UriTemplate.of(pedidoUrl, filtroVariables), rel);
+
 	}
 
 }
--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.openapi.controller.EstatisticasControllerOpenApi;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
@@ -26,6 +28,19 @@ public class EstatisticasController implements EstatisticasControllerOpenApi{
 
 	@Autowired
 	private VendaReportService vendaReportService;
+	
+	@Autowired
+	AlgaLinks algaLinks;
+	
+	@Override
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public EstatisticasModel estatisticas() {
+		var estatisticasModel = new EstatisticasModel();
+		
+		estatisticasModel.add(algaLinks.linkToEstatisticasVendasDiarias("vendas-diarias"));
+		
+		return estatisticasModel;
+	}
 
 	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
@@ -42,6 +57,10 @@ public class EstatisticasController implements EstatisticasControllerOpenApi{
 		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
 
 		return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).headers(headers).body(bytesPdf);
+	}
+	
+	public static class EstatisticasModel extends RepresentationModel<EstatisticasModel> {
+		
 	}
 
 }
