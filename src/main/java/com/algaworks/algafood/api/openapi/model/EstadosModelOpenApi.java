b75fb623078ea49939d8f0b88f356fd137127685
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-08-23 21:02:52 -0300

    19.42. Desafio: corrigindo a documentação dos endpoints de estados (Resolvido)

---
+++ src/main/java/com/algaworks/algafood/api/openapi/model/EstadosModelOpenApi.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.api.openapi.model;
+
+import java.util.List;
+
+import org.springframework.hateoas.Links;
+
+import com.algaworks.algafood.api.model.EstadoModel;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+@ApiModel("EstadosModel")
+@Data
+public class EstadosModelOpenApi {
+
+	private EstadoEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	
+	@ApiModel("EstadosEmbeddedModel")
+	@Data
+	public class EstadoEmbeddedModelOpenApi {
+		private List<EstadoModel> estados;
+	}
+}
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -26,9 +26,11 @@ import org.springframework.web.context.request.ServletWebRequest;
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.CozinhaModel;
+import com.algaworks.algafood.api.model.EstadoModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.openapi.model.CidadesModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
+import com.algaworks.algafood.api.openapi.model.EstadosModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PedidosResumoModelOpenApi;
@@ -80,6 +82,10 @@ public class SpringFoxConfig {
 			.alternateTypeRules(AlternateTypeRules.newRule(
 					typeResolver.resolve(CollectionModel.class, CidadeModel.class),
 					CidadesModelOpenApi.class
+					))
+			.alternateTypeRules(AlternateTypeRules.newRule(
+					typeResolver.resolve(CollectionModel.class, EstadoModel.class),
+					EstadosModelOpenApi.class
 					))
 			.alternateTypeRules(AlternateTypeRules.newRule(
 					typeResolver.resolve(Page.class, PedidoResumoModel.class),
