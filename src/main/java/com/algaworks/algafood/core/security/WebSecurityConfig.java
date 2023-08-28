0195b6e41a0360cf26af8c7d0241f5a72dd02cde
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-08-28 18:17:40 -0300

    22.3. Configurando Spring Security com HTTP Basic

---
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.and()
+			.authorizeHttpRequests()
+			.antMatchers("/v1/cozinhas/**").permitAll()
+			.anyRequest().authenticated()
+		
+			.and()
+				.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+					
+			.and()
+				.csrf().disable();
+	}
+}
