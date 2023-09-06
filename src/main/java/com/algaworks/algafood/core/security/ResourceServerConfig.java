d4ea00a992a51ba367f082d5f1076baabb3b6ffd
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-05 21:47:17 -0300

    23.13. Revisando o fluxo de aprovação do Authorization Code com JWT

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,13 +1,8 @@
 package com.algaworks.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
@@ -24,11 +19,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			
 	} 
 	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec("l2kj3lkj2l3j2lkj3lk2j3lkj2l3j2lj3lk2j3lj3l232lj3l2j3qqq".getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
+	
 	
 }
