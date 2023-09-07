a5e69e07f609ffa30600638035e0cc14cac11d63
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-07 11:32:14 -0300

    23.22. Desafio: tratando AcessDeniedException no ExceptionHandler (Resolvido)

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -11,7 +11,8 @@ public enum ProblemType {
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
 	PARAMETRO_INVALIDO("/parametro-invalido","Parâmetro Inválido"),
-	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
+	ACESSO_NEGADO("/acesso-negado", "Acesso negado");
 	
 
 	private String title;
