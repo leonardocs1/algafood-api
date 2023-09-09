46b921f6d0c6ffff19f5befc28e6db81a04b3b31
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-09 18:58:28 -0300

    23.29. Restringindo acessos com @Postuthorize

--- src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
@@ -27,6 +27,7 @@ import com.algaworks.algafood.api.v1.openapi.controller.PedidoControllerOpenApi;
 import com.algaworks.algafood.core.data.PageWrapper;
 import com.algaworks.algafood.core.data.PageableTranslator;
 import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.filter.PedidoFilter;
@@ -73,6 +74,7 @@ public class PedidoController implements PedidoControllerOpenApi {
 		return pagedResourcesAssembler.toModel(pedidosPage, pedidoResumoModelAssembler);
 	}
 
+	@CheckSecurity.Pedidos.PodeBuscar
 	@GetMapping("/{codigoPedido}")
 	public PedidoModel buscar(@PathVariable String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
--- src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
@@ -27,4 +27,5 @@ public class AlgaSecurity {
 	public boolean gerenciaRestaurante(Long restauranteId) {
 		return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
 	}
+	
 }
