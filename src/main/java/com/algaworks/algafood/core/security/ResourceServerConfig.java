595a11d3588b42930a5e177791f78f94535ddbe3
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-09-13 10:08:49 -0300

    23.41. Juntando o Resource Server com o Authorization Server no mesmo projeto

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +23,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.formLogin()
+			.and()
+			.authorizeRequests()
+			.antMatchers("/oauth/**").authenticated()
+			.and()
 			.csrf().disable()
 			.cors().and()
 			.oauth2ResourceServer()
@@ -49,6 +56,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		});
 		
 		return jwtAutheticationConverter;
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 	
 }
