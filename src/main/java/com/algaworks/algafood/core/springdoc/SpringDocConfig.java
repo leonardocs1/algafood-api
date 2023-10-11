c428a6d62eceed676fef066a6cd77c1062cb230a
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-11 10:16:16 -0300

    26.11. Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -7,12 +7,16 @@ import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
 
 import java.util.Arrays;
 
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -48,5 +52,27 @@ public class SpringDocConfig {
                 		)
                 	);
     }
+    
+    @Bean
+    public OpenApiCustomiser openApiCustomiser() {
+    	return openApi -> {
+    		openApi.getPaths()
+    			.values()
+    			.stream()
+    			.flatMap(pathItem -> pathItem.readOperations().stream())
+    			.forEach(operation -> {
+    				ApiResponses responses = operation.getResponses();
+    				
+    				ApiResponse apiResponseNaoEncontrado = new ApiResponse().description("Recurso não encontrado");
+    				ApiResponse apiResponsesErroInterno = new ApiResponse().description("Erro interno no servidor");
+    				ApiResponse apiReponseSemRepresentacao = new ApiResponse().description("Recurso não possui uma representação que poderia ser aceita pelo consumidor");
+    				
+    				responses.addApiResponse("500", apiResponsesErroInterno);
+    				responses.addApiResponse("404", apiResponseNaoEncontrado);
+    				responses.addApiResponse("406", apiReponseSemRepresentacao);
+    			});
+    	};
+    }
+    
 
 }
