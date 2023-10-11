c8eee854a2c5083f8873a39c0f73c1e88e45c7c6
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-10 21:27:35 -0300

    26.6. Descrevendo tags na documentação e associando com controllers

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -9,6 +9,10 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.tags.Tag;
+
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,7 +43,10 @@ public class SpringDocConfig {
                 ).externalDocs(new ExternalDocumentation()
                         .description("AlgaWorks")
                         .url("https://algaworks.com")
-                );
+                ).tags(Arrays.asList(
+                	new Tag().name("Cidades").description("Gerencia as cidades")	
+                		)
+                	);
     }
 
 }
