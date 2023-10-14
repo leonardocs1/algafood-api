d78924f1bca858ad581b6fb73c1069d74fdb0792
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-14 15:56:40 -0300

    26.18. Desafio: descrevendo documentação de enpoints de cozinhas (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/model/CozinhaModel.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/CozinhaModel.java
@@ -6,6 +6,7 @@ import org.springframework.hateoas.server.core.Relation;
 import com.algaworks.algafood.api.v1.model.view.RestauranteView;
 import com.fasterxml.jackson.annotation.JsonView;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +15,11 @@ import lombok.Setter;
 @Getter
 public class CozinhaModel extends RepresentationModel<CozinhaModel>{
 
+	@Schema(example = "1")
 	@JsonView(RestauranteView.Resumo.class)
 	private Long id;
 	
+	@Schema(example = "Maranhense")
 	@JsonView(RestauranteView.Resumo.class)
 	private String nome;
 }
--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
@@ -7,20 +7,29 @@ import com.algaworks.algafood.api.v1.model.CozinhaModel;
 import com.algaworks.algafood.api.v1.model.input.CozinhaInput;
 import com.algaworks.algafood.core.springdoc.PageableParameter;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Cozinhas")
 public interface CozinhaControllerOpenApi {
 
+	@Operation(summary = "Lista as cozinhas")
 	@PageableParameter
 	PagedModel<CozinhaModel> listar(@Parameter(hidden = true) Pageable pageable);
 
-	CozinhaModel buscar(Long cozinhaId);
+	@Operation(summary = "Busca uma cozinha por ID")
+	CozinhaModel buscar(@Parameter(description = "ID de uma cozinha", example = "1", required = true) Long cozinhaId);
 
-	CozinhaModel adicionar(CozinhaInput cozinhaInput);
+	@Operation(summary = "Adiciona uma nova cozinha")
+	CozinhaModel adicionar(@RequestBody(description = "Representação de uma cozinha", required = true) CozinhaInput cozinhaInput);
 
-	CozinhaModel atualizar(Long cozinhaId, CozinhaInput cozinhaInput);
+	@Operation(summary = "Atualiza uma cozinha por ID")
+	CozinhaModel atualizar(@Parameter(description = "ID de uma cozinha", example = "1", required = true) Long cozinhaId, @RequestBody(description = "Representação de uma cozinha com os dados atualizados", required = true) CozinhaInput cozinhaInput);
 
-	void remover(Long cozinhaId);
+	@Operation(summary = "Excluí uma cozinha por ID")
+	void remover(@Parameter(description = "ID de uma cozinha", example = "1", required = true) Long cozinhaId);
 }
