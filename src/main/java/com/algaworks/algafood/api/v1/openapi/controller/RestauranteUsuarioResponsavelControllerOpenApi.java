bae41a357f0ee44397231370f040dbae214279f7
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-16 18:01:21 -0300

    26.28. Desafio: descrevendo documentação de endpoints de associação de restaurante com usuários (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -5,15 +5,27 @@ import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.v1.model.UsuarioModel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Restaurantes")
 public interface RestauranteUsuarioResponsavelControllerOpenApi {
 
-	CollectionModel<UsuarioModel> listar(Long restauranteId);
+	@Operation(summary = "Lista os usuários responsáveis associados ao restaurante")
+	CollectionModel<UsuarioModel> listar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true) Long restauranteId);
 
-	ResponseEntity<Void> associar(Long restauranteId, Long usuarioId);
+	@Operation(summary = "Associação de restaurante com usuário responsável")
+	ResponseEntity<Void> associar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@Parameter(description = "ID do usuário responsável", example = "1", required = true) Long usuarioId);
 
-	ResponseEntity<Void> desassociar(Long restauranteId, Long usuarioId);
+	@Operation(summary = "Desassociação de restaurnte com usuário responsável")
+	ResponseEntity<Void> desassociar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@Parameter(description = "ID do usuário responsável", example = "1", required = true) Long usuarioId);
 
 }
