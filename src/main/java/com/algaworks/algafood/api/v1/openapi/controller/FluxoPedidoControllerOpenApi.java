3711a967bcca4578fb5547797e6d393f935fe166
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-16 16:48:29 -0300

    26.26. Desafio: descrevendo documentação de endpoints de fluxo de pedidos (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/FluxoPedidoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -2,14 +2,21 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Pedidos")
 public interface FluxoPedidoControllerOpenApi {
 
-	ResponseEntity<Void> confirmar(String codigoPedido);
+	@Operation(summary = "Registra confirmação de pedido")
+	ResponseEntity<Void> confirmar(@Parameter(description = "Código de pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 
-	ResponseEntity<Void> cancelar(String codigoPedido);
+	@Operation(summary = "Registra cancelamento de pedido")
+	ResponseEntity<Void> cancelar(@Parameter(description = "Código de pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 
-	ResponseEntity<Void> entregar(String codigoPedido);
+	@Operation(summary = "Registra entrega de pedido")
+	ResponseEntity<Void> entregar(@Parameter(description = "Código de pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 }
