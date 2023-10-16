52576a504468d86c70b1b8755eb9ddd3cc6f0e08
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-16 17:07:46 -0300

    26.27. Desafio: descrevendo documentação de endpoints de restaurantes com formas de pagamento (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -5,15 +5,26 @@ import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.v1.model.FormaPagamentoModel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Restaurantes")
 public interface RestauranteFormaPagamentoControllerOpenApi {
 
-	CollectionModel<FormaPagamentoModel> listar(Long restauranteId);
+	@Operation(summary = "Lista as formas de pagamentos associadas ao restaurante")
+	CollectionModel<FormaPagamentoModel> listar(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true) Long restauranteId);
 
-	ResponseEntity<Void> associar(Long restauranteId, Long formaPagamentoId);
+	@Operation(summary = "Associação de restaurante com forma de pagamento")
+	ResponseEntity<Void> associar(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true) Long restauranteId,
+			@Parameter(description = "ID de uma forma de pagamento", example = "1", required = true) Long formaPagamentoId);
 
-	ResponseEntity<Void> desassociar(Long restauranteId, Long formaPagamentoId);
-
+	@Operation(summary = "Desassociação de restaurante com forma de pagamento")
+	ResponseEntity<Void> desassociar(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true) Long restauranteId,
+			@Parameter(description = "ID de uma forma de pagamento", example = "1", required = true) Long formaPagamentoId);
 }
