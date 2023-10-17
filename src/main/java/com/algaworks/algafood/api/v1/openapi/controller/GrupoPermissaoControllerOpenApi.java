ef7936103b81d2ea1bf4734bcb672343e6ac62ad
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-17 17:45:57 -0300

    26.31. Desafio: descrevendo documentação de endpoints de associação de permissões com grupos (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/GrupoPermissaoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/GrupoPermissaoControllerOpenApi.java
@@ -5,15 +5,26 @@ import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.v1.model.PermissaoModel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Grupos")
 public interface GrupoPermissaoControllerOpenApi {
 
-	CollectionModel<PermissaoModel> listar(Long grupoId);
+	@Operation(summary = "Lista as permissões associadas a um grupo")
+	CollectionModel<PermissaoModel> listar(
+			@Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId);
 
-	ResponseEntity<Void> associar(Long grupoId, Long permissaoId);
+	@Operation(summary = "Associação de permissão com grupo")
+	ResponseEntity<Void> associar(@Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId,
+			@Parameter(description = "ID da permissão", example = "1", required = true) Long permissaoId);
 
-	ResponseEntity<Void> dessasociar(Long grupoId, Long permissaoId);
+	@Operation(summary = "Desassociação de permissão com grupo")
+	ResponseEntity<Void> dessasociar(
+			@Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId,
+			@Parameter(description = "ID da permissão", example = "1", required = true) Long permissaoId);
 
 }
