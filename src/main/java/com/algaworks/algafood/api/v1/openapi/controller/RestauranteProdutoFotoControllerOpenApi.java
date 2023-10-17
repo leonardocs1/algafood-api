ff985ec1450c12b6b439effbf603f8d6414e2faa
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-16 23:14:51 -0300

    26.30. Desafio: descrevendo documentação de endpoints de fotos de produtos (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -16,30 +16,33 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Produtos")
 public interface RestauranteProdutoFotoControllerOpenApi {
 
 	@Operation(summary = "Atualiza a foto do produto de um restaurante")
-	FotoProdutoModel atualizarFoto(@Parameter(description = "ID de um restaurante", example = "1", required = true) Long restauranteId, 
-								   @Parameter(description = "ID de um produto", example = "2", required = true) Long produtoId, 
-								   @RequestBody(required = true) FotoProdutoInput fotoProdutoInput,
-			MultipartFile arquivo) throws IOException;
-
-	public void remover(Long restauranteId, Long produtoId);
+	FotoProdutoModel atualizarFoto(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true) Long restauranteId,
+			@Parameter(description = "ID de um produto", example = "2", required = true) Long produtoId,
+			@RequestBody(required = true) FotoProdutoInput fotoProdutoInput, MultipartFile arquivo) throws IOException;
 
 	@Operation(summary = "Busca a foto do produto de um restaurante", responses = {
 			@ApiResponse(responseCode = "200", content = {
 					@Content(mediaType = "application/json", schema = @Schema(implementation = FotoProdutoModel.class)),
 					@Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
-					@Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
-			}
-					
-		)
-	})
-	FotoProdutoModel buscar(Long restauranteId, Long produtoId);
+					@Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary")) }
+
+			) })
+	FotoProdutoModel buscar(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true) Long restauranteId,
+			@Parameter(description = "ID de um produto", example = "2", required = true) Long produtoId);
 
 	@Operation(hidden = true)
 	ResponseEntity<?> servir(Long restauranteId, Long produtoId, String acceptHeader)
 			throws HttpMediaTypeNotAcceptableException;
+
+	@Operation(summary = "Exclui a foto do produto de um restaurante")
+	void remover(Long restauranteId, Long produtoId);
 }
