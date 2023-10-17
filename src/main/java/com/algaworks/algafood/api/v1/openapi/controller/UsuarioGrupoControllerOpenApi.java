41d9e84980a9f82642b60759861e14dbbb740c80
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-17 18:43:36 -0300

    26.33. Desafio: descrevendo documentação de endpoints de associação de grupos com usuários (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioGrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioGrupoControllerOpenApi.java
@@ -5,15 +5,27 @@ import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.v1.model.GrupoModel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Usuários")
 public interface UsuarioGrupoControllerOpenApi {
 
-	CollectionModel<GrupoModel> listar(Long usuarioId);
+	@Operation(summary = "Lista os grupos associados a um usuário")
+	CollectionModel<GrupoModel> listar(
+			@Parameter(description = "ID de um usuário", example = "1", required = true) Long usuarioId);
 
-	ResponseEntity<Void> associar(Long usuarioId, Long grupoId);
+	@Operation(summary = "Associação de grupo com usuário")
+	ResponseEntity<Void> associar(
+			@Parameter(description = "ID de um usuário", example = "1", required = true) Long usuarioId,
+			@Parameter(description = "ID de um grupo", example = "1", required = true) Long grupoId);
 
-	ResponseEntity<Void> desassociar(Long usuarioId, Long grupoId);
+	@Operation(summary = "Desassociação de grupo com usuário")
+	ResponseEntity<Void> desassociar(
+			@Parameter(description = "ID de um usuário", example = "1", required = true) Long usuarioId,
+			@Parameter(description = "ID de um grupo", example = "1", required = true) Long grupoId);
 
 }
