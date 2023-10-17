d8b78db1611860b90a7b03cbaf820eba73c6f9c4
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-17 18:20:50 -0300

    26.32. Desafio: descrevendo documentação de endpoints de usuários (Resolvido)

--- src/main/java/com/algaworks/algafood/api/v1/model/input/SenhaInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/SenhaInput.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.v1.model.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +11,11 @@ import lombok.Setter;
 @Getter
 public class SenhaInput {
 
+	@Schema(example = "1234")
 	@NotBlank
 	private String senhaAtual;
 
+	@Schema(example = "1234")
 	@NotBlank
 	@Size(min = 4, max = 12)
 	private String novaSenha;
--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
@@ -7,18 +7,32 @@ import com.algaworks.algafood.api.v1.model.input.SenhaInput;
 import com.algaworks.algafood.api.v1.model.input.UsuarioComSenhaInput;
 import com.algaworks.algafood.api.v1.model.input.UsuarioInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Usuários")
 public interface UsuarioControllerOpenApi {
 
+	@Operation(summary = "Lista os usuários")
 	CollectionModel<UsuarioModel> listar();
 
-	UsuarioModel buscar(Long usuarioId);
+	@Operation(summary = "Busca o usuário por ID")
+	UsuarioModel buscar(@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId);
 
-	UsuarioModel adicionar(UsuarioComSenhaInput usuarioInput);
+	@Operation(summary = "Cadastra um usuário")
+	UsuarioModel adicionar(@RequestBody(description = "Representação de um novo usuário", required = true) UsuarioComSenhaInput usuarioInput);
 
-	UsuarioModel atualizar(Long usuarioId, UsuarioInput usuarioInput);
+	@Operation(summary = "Atualiza um usuário por ID")
+	UsuarioModel atualizar( 
+			@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId, 
+			@RequestBody(description = "Representação de um usuário com os novos dados", required = true) UsuarioInput usuarioInput);
 
-	void atualizarSenha(Long usuarioId, SenhaInput senhaInput);
+	@Operation(summary = "Atualiza a senha de um usuário")
+	void atualizarSenha(
+			@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId, 
+			@RequestBody(description = "Representação de um nova senha", required = true) SenhaInput senhaInput);
 }
