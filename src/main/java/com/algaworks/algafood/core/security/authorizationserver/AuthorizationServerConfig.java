16c2469aacd43f4eba3d850eba3db66e35cd0c44
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-18 16:29:53 -0300

    27.9. Configurando a geração de Access Token JWT no Authorization Server

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -1,9 +1,16 @@
 package com.algaworks.algafood.core.security.authorizationserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.time.Duration;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
@@ -20,8 +27,11 @@ import org.springframework.security.oauth2.server.authorization.config.ProviderS
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.time.Duration;
-import java.util.Arrays;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 public class AuthorizationServerConfig {
@@ -45,18 +55,33 @@ public class AuthorizationServerConfig {
 				.clientSecret(passwordEncoder.encode("backend123"))
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS).scope("READ")
-				.tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+				.tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
 						.accessTokenTimeToLive(Duration.ofMinutes(30)).build())
 				.build();
 
 		return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodbackend));
 	}
- 
+
 	@Bean
 	public OAuth2AuthorizationService oAuth2AuthorizationService(JdbcOperations jdbcOperations,
 			RegisteredClientRepository registeredClientRepository) {
 
 		return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
+	}
+
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(JwtKeyStoreProperties properties) throws Exception {
+		char[] keyStorePass = properties.getPassword().toCharArray();
+		String keypairAlias = properties.getKeypairAlias();
+
+		Resource jksLocation = properties.getJksLocation();
+		InputStream inputStream = jksLocation.getInputStream();
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		keyStore.load(inputStream, keyStorePass);
+
+		RSAKey rsaKey = RSAKey.load(keyStore, keypairAlias, keyStorePass);
+
+		return new ImmutableJWKSet<>(new JWKSet(rsaKey));
 	}
 
 }
