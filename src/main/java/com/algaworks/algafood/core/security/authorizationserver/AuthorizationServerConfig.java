263bfc1a90a7a5d156ce1ed7ef80a8ffe432dcc8
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-10-18 15:25:37 -0300

    27.6. Configurando o Resource Server com token opaco

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -1,10 +1,9 @@
 package com.algaworks.algafood.core.security.authorizationserver;
 
-import java.time.Duration;
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,37 +17,43 @@ import org.springframework.security.oauth2.server.authorization.config.ProviderS
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 @Configuration
 public class AuthorizationServerConfig {
 
-	@Bean
-	public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
-		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-		return http.build();
-	}
-	
-	@Bean
-	public ProviderSettings providerSettings(AlgaFoodSecurityProperties properties) {
-		return ProviderSettings.builder()
-				.issuer(properties.getProviderUrl())
-				.build();
-	}
-	
-	public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
-		
-		RegisteredClient algafoodbackend = RegisteredClient 
-				.withId("1")
-				.clientId("algafood-backend")
-				.clientSecret(passwordEncoder.encode("backend123"))
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.scope("READ")
-				.tokenSettings(TokenSettings.builder()
-						.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
-						.accessTokenTimeToLive(Duration.ofMinutes(30))
-						.build())
-				.build();
-				
-		return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodbackend));
-	}
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.build();
+    }
+
+    @Bean
+    public ProviderSettings providerSettings(AlgaFoodSecurityProperties properties) {
+        return ProviderSettings.builder()
+                .issuer(properties.getProviderUrl())
+                .build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+
+        RegisteredClient algafoodbackend = RegisteredClient
+                .withId("1")
+                .clientId("algafood-backend")
+                .clientSecret(passwordEncoder.encode("backend123"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope("READ")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                        .accessTokenTimeToLive(Duration.ofMinutes(30))
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodbackend));
+    }
+
 }
